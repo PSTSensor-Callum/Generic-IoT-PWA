827f250bdd74876b3a6a9695a2525e1ddfd923e4
Callum Kirkwood <callum.kirkwood@greenlordstudios.co.uk>
2023-07-10 10:11:18 +0100

    Controller

--- Generic-IoT-PWA/Data/PWADbContext.cs
+++ Generic-IoT-PWA/Data/PWADbContext.cs
@@ -6,5 +6,7 @@ namespace Generic_IoT_PWA.Data
     public class PWADbContext : DbContext
     {
         public DbSet<GenericSensor> GenericSensors { get; set; }
+
+        public PWADbContext(DbContextOptions<PWADbContext> options) : base(options) { }
     }
 }
